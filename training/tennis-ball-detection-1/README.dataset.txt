b2bdaab095a6d59054c82a47549680b73db82914
tomcoder1 <ptommy566@gmail.com>
2026-04-25 20:44:01 +0700

    back

--- training/tennis-ball-detection-1/README.dataset.txt
+++
@@ -1,7 +0,0 @@
-# undefined > Dataset1V1-16-09-2021
-https://public.roboflow.ai/object-detection/undefined
-
-Provided by undefined
-License: CC BY 4.0
-
-undefined
--- yolo_inference.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8x')
-
-result = model.predict('input-data/short-clip.mp4', save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
